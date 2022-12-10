68b62f5df478a2948f923b4a1427ad1c4f6cd8cf
0xNekr <0xNekr.dev@gmail.com>
2022-12-10 17:19:39 +0100

    day 10

---
+++ contracts/Jour10/NekrIsERC721_Jour10.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.17;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
+import "@openzeppelin/contracts/utils/Strings.sol";
+import "@openzeppelin/contracts/utils/cryptography/MerkleProof.sol";
+
+contract NekrIsERC721_Jour10 is ERC721, Ownable {
+
+    constructor() ERC721 ('', '') {}
+}
